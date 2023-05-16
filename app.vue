ca46e2a7e4504eef5ae1b444277b77fa8ba67f96
opennanoor <133144205+opennanoor@users.noreply.github.com>
2023-05-16 19:08:01 +0200

    more SEO

--- app.vue
+++ app.vue
@@ -5,7 +5,19 @@ import Index from './pages/index.vue'
 
 </script>
 <template>
+  <html lang="en">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="icon" href="/favicon.ico" />
+  <meta name="description" content="Poppy is a community driven token on the Ethereum Network.">
+  <meta name="keywords" content="Poppy, Poppy Token, Poppy Coin, Poppy Crypto, Poppy Crypto Currency, Poppy Token Eth, ">
+  <meta name="author" content="Poppy Token">
+  <meta name="robots" content="index, follow">
+  <meta property="og:title" content="Poppy Token">
+  <meta property="og:description" content="Poppy Token is a community driven token on the Binance Smart Chain.">
   <ClientOnly>
     <Index />
   </ClientOnly>
+
+  </html>
 </template>
