a1ad19aaa24d3bb16e1c09e558ef5167331278bf
opennanoor <133144205+opennanoor@users.noreply.github.com>
2023-05-19 20:30:22 +0200

    add cors and prerender /

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,12 @@
 export default defineNuxtConfig({
     ssr: true,
 
+    routeRules: {
+        '/': { prerender: true },
+        // Add corsheaders on API routes
+        '/*': { cors: true },
+    },
+
     plugins: [{ src: "/plugins/aos.client.ts", ssr: false, mode: "client" },
     { src: "/plugins/router.scrollBehaviour.ts", ssr: false, mode: "client" }
     ],
