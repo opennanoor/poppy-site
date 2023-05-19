1f4a28932f1ddf85e62d946232da4c6a9a2fff5a
opennanoor <133144205+opennanoor@users.noreply.github.com>
2023-05-19 20:31:14 +0200

    ssr

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,8 +7,9 @@ export default defineNuxtConfig({
         '/*': { cors: true },
     },
 
-    plugins: [{ src: "/plugins/aos.client.ts", ssr: false, mode: "client" },
-    { src: "/plugins/router.scrollBehaviour.ts", ssr: false, mode: "client" }
+    plugins: [
+        { src: "/plugins/aos.client.ts", mode: "client" },
+        { src: "/plugins/router.scrollBehaviour.ts", mode: "client" }
     ],
 
     modules: ['nuxt-icon', '@nuxt/image-edge', '@vueuse/nuxt'],
